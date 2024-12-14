cac03ab541172c3a002d6554024690dab53be29e
JunSeok Lee <junseoklee.kr@gmail.com>
2024-12-14 23:50:00 +0900

    Time: 6 ms (25.4%), Space: 42.2 MB (56.61%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean isPathCrossing(String path) {
 
-        int l = path.length();
+        int x=0, y=0;
 
-        int x=0; int y=0;
-
-        for(int i=0; i<l; i++){
-            String temp = path.subString(i);
-            if(temp = 'N'){
-                y++;
-            }else if(temp = 'W'){
-                x--;
-            }else if(temp = 'E')
-        }
+        // 방문경로를 저장한다.
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0"); // 초기 위치
         
+        for(char direction : path.toCharArray()){
+            if(direction == 'N'){
+                y++;
+            }else if(direction == 'W'){
+                x--;
+            }else if(direction == 'E'){
+                x++;
+            }else if(direction == 'S'){
+                y--;
+            }
+
+            // 현재 위치 추가 
+            String currentPosition = x + "," + y;
+            
+            // 지금 위치를 온적이 있다면 true 반환.
+            if(visited.contains(currentPosition)){
+                return true;
+            }
+
+            // 온적 없다면 추가
+            visited.add(currentPosition);
+        }
+        return false;
     }
 }
-
-
-
-/**
-1. 스트링을 무슨 타입으로 받아야할까
-2. N일때, E일때, S, W일때 각각 포인트가 어떻게 달라지는지 (IF문으로 분개처리)
-3. 
- */
