6ccf16a01f741b2eefd8c24cd87bb223a3191236
JunSeok Lee <junseoklee.kr@gmail.com>
2025-03-04 21:42:30 +0900

    Time: 3 ms (57.62%), Space: 53.3 MB (37.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int max = 0;
-
-        for(int num: nums){
-            if(num > max) max = num;
-        }
-
-        int check[] = new int[max+1];
-
-        for(int num: nums){
-            check[num]++;
-        }
-
-        for(int i=0; i<check.length; i++){
-            if(check[i]> (n/2)){
-                return i;
-            }
-        }
-        return 0;
         
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        return nums[n/2];
+
     }
 }
