3e5bf8d6bc3eaa756863dad795a2685ef1601521
JunSeok Lee <junseoklee.kr@gmail.com>
2025-04-21 20:28:33 +0900

    Update CyclicRotation.java

--- Codility/CyclicRotation.java
+++ Codility/CyclicRotation.java
@@ -1,28 +1,13 @@
 class Solution {
     public int[] solution(int[] A, int K) {
+        
+        int N = A.length;
+        int result[]= new int[N];
 
-        int size = A.length;
-        if(size==0){
-            return A;
+        for(int i=0; i<N; i++){
+            result[(i+K)%N] = A[i];
         }
-        int temp[] = new int[size]; // 새로운 rotation 결과
 
-        // K번 반복
-        for(int i=0; i<K%size; i++){
-
-            // rotation
-            temp[0] = A[size-1]; //A의 맨 마지막 값이 temp의 첫번째 값이 된다.
-
-            // n번재 값은 n+1에 배치한다.(0번째 값은 1번째 값이 된다.)
-            for(int j=0; j<size-1; j++){
-                temp[j+1] = A[j];
-            }
-
-            // 변경된 배열 내용을 다시 A에 반영.
-            for(int j=0; j<size; j++){
-                A[j] = temp[j];
-            }
-        }
-        return A;
+        return result;
     }
 }
