7be4992a7152dc004370e423d50cc01947383668
JunSeok Lee <junseoklee.kr@gmail.com>
2025-04-27 20:32:58 +0900

    Create Solution.java

---
+++ Codility/Distinct/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] A) {
+        HashSet<Integer> arr = new HashSet<>();
+
+        for(int i : A){
+            arr.add(i);
+        }
+
+        return arr.size();
+    }
+}
