24ece12fde251d311fc002ab697590558365f2bd
JunSeok Lee <junseoklee.kr@gmail.com>
2025-05-12 20:05:30 +0900

    Update Solution.java

--- Codility/MaxSliceSum/Solution.java
+++ Codility/MaxSliceSum/Solution.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[] A) {
         
         int currentSum = 0;
-        int maxSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
         for(int i : A){
             currentSum = Math.max(i, currentSum + i);
