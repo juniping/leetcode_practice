f524ef42aab4983548d2e56ad8cd6c2432ffe941
JunSeok Lee <junseoklee.kr@gmail.com>
2025-05-26 20:33:20 +0900

    Time: 2 ms (97.23%), Space: 41.7 MB (81.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for(char c: s.toCharArray()){
-            if(c == '('){
-                stack.push(')');
-            }else if (c == '{'){
-                stack.push('}');
-            }else if (c == '['){
-                stack.push(']');
-            }else if (stack.isEmpty() || stack.pop() != c){
-                return false;
+
+        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+
+        for(char c : s.toCharArray()){
+            if(c=='('||c=='['||c=='{'){
+                stack.push(c);
+            }else {
+                if (!stack.isEmpty()){
+                    char temp = stack.pop();
+                    if(temp!=map.get(c)){
+                        return false;
+                    }
+                }else{
+                    return false; // stack이 비었는데 닫는 괄호가 나온 경우 
+                }
             }
         }
-        return stack.isEmpty();
+        if(!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;        
     }
 }
