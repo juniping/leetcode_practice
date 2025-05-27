d15cac38e4ce5e514cda57e049adb5ae5a568526
JunSeok Lee <junseoklee.kr@gmail.com>
2025-05-27 21:56:29 +0900

    Time: 0 ms (100%), Space: 43.4 MB (11.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,20 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
 
-        if(nums[0]>target){
-            return 0;
-        }
+        int properIndex = 0;
 
-        int c = 0;
-        int result = 0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i]<target){
-                c = i;
-            }
+            properIndex = i;
+
             if(nums[i]==target){
                 return i;
             }
+
+            if(target < nums[i]){
+                break;
+            }
+        }        
+        if(target > nums[properIndex]){
+            return properIndex+1;
         }
-        return c+1;
+        return properIndex;
     }
 }
