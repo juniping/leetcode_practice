dec5e48fdd8ee5799613be9e2b3270dc23eb2841
JunSeok Lee <junseoklee.kr@gmail.com>
2025-06-03 00:48:30 +0900

    Time: 2 ms (25.42%), Space: 42.9 MB (11.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,27 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         String result = "";
-        int len = Math.min(word1.length(), word2.length());
-
-
+        int len = Math.max(word1.length(), word2.length());
 
         for(int i=0; i<len; i++){
-            result+= word1.substring(i,i+1);
-            result+= word2.substring(i,i+1);
-        }
-
-        if(len == word1.length()){
-            for(int i=len; i<word2.length(); i++){
-                result+= word2.substring(i,i+1);
-            }
-        }else{
-            for(int i=len; i<word1.length(); i++){
+            if(i < word1.length()){
                 result+= word1.substring(i,i+1);
             }
+            if(i < word2.length()){
+                result+= word2.substring(i,i+1);
+            }            
         }
-
         return result;
-
-
     }
 }
