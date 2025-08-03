a342069ab88e5e715a1643136d27dea793983efd
JunSeok Lee <junseoklee.kr@gmail.com>
2025-08-03 13:05:43 +0900

    Time: 2 ms (98.03%), Space: 42.3 MB (15.91%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i:arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i: map.values()){
+            set.add(i);
+        }
+
+        return map.size() == set.size();
+    }
+}
